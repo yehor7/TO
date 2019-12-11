9f87d689f2ad56b83b2d4622923a4e7640b0397b
yehor7 <38106414+yehor7@users.noreply.github.com>
2019-12-11 16:54:33 +0100

    Update Prototype.java

--- Prototype/Prototype.java
+++ Prototype/Prototype.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.opp.Prototype;
 
-/**
- *
- * @author 4
- */
 public abstract class Prototype implements Cloneable {
     
     abstract Prototype copyMe() throws CloneNotSupportedException;

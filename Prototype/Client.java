3ba16ed528574cc6e01f39c636b0447ce931a232
yehor7 <38106414+yehor7@users.noreply.github.com>
2019-12-11 16:55:11 +0100

    Update Client.java

--- Prototype/Client.java
+++ Prototype/Client.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.opp.Prototype;
 
-/**
- *
- * @author 4
- */
 public class Client {
   private Prototype prototype;
   public Client(Prototype prototype) {

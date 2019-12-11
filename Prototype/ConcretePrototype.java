087d95e80103f336c6846e41f56a8ea216140923
yehor7 <38106414+yehor7@users.noreply.github.com>
2019-12-11 16:55:39 +0100

    Update ConcretePrototype.java

--- Prototype/ConcretePrototype.java
+++ Prototype/ConcretePrototype.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.opp.Prototype;
 
-/**
- *
- * @author 4
- */
 public class ConcretePrototype extends Prototype {
     public Prototype copyMe() throws CloneNotSupportedException {
     return (Prototype) this.clone();
